ca7202f4909f07bca63cb9161e7ced977a34d45b
Tarex <tarajakub2003@gmail.com>
2025-04-07 09:21:10 +0200

    Podstawy klasy TestObject i głównego okna programu

---
+++ MIWProjekt/TestObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIWProjekt
+{
+
+    class TestObject
+    {
+        static Random rand = new Random();
+        public List<bool> chromoSet = new List<bool>();
+        public double FitValue;
+
+        public TestObject(int MaxBits)
+        {
+            for (int i = 0; i < MaxBits; i++)
+            {
+                
+            }
+        }
+
+        public TestObject(List<bool> chromosomes)
+        {
+            this.chromoSet = chromosomes;
+        }
+
+        public TestObject Clone()
+        {
+            return new TestObject(chromoSet);
+        }
+        static TestObject TournamentSelection(List<TestObject> generation, int tournamentSize)
+        {
+            var tournament = new List<TestObject>();
+            for (int i = 0; i < tournamentSize; i++)
+            {
+                tournament.Add(generation[rand.Next(generation.Count)]);
+            }
+            return tournament.OrderByDescending(obj => obj.FitValue).First().Clone();
+        }
+    }
+}
+
